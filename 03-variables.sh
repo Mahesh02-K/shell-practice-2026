6a021f36963be418d8089b668189f18e69d79e68
Mahesh02-K <kakuturumahesh@gmail.com>
2026-04-29 14:24:06 +0530

    Variables Examole

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=Trump
+Person2=Musk
+
+echo "$Person1 :: Hey $Person2! how are you?"
+echo "$Person2 :: Hi DJT! I'm good, how about you?"
+echo "$Person1 :: good! what's going on?"
+echo "$Person2 :: nothing! just planning about mars trip."
+echo "$Person1 :: Oh! super, carry on."
