e55794df74f2cd0e1c33e44d8d93cf070d0832e0
Mahesh02-K <kakuturumahesh@gmail.com>
2026-04-29 14:27:22 +0530

    Variables Examole

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1 :: Hey $Person2! how are you?"
+echo "$Person2 :: Hi mate! I'm good, how about you?"
+echo "$Person1 :: good! what's going on?"
+echo "$Person2 :: nothing! just planning about mars trip."
+echo "$Person1 :: Oh! super, carry on."
