400ef56a11bd14b0edac359dd02d16f77abda3ab
Mahesh02-K <kakuturumahesh@gmail.com>
2026-04-29 14:18:27 +0530

    conversation b/w two people

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump :: Hey Musk! how are you?"
+echo "Musk :: Hi DJT! I'm good, how about you?"
+echo "Trump :: good! what's going on?"
+echo "Musk :: nothing! just planning about mars trip."
+echo "Trump :: Oh! super, carry on."
