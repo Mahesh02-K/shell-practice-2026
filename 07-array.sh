0cb4dbadc02d112c276ef5e029cfbe1ce4f90e75
Mahesh02-K <kakuturumahesh@gmail.com>
2026-04-29 14:47:41 +0530

    Variables Examole

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+RIVERS=("KRISHNA" "GODAVARI" "PENNA" "GANGA")
+
+echo "First river :: ${RIVERS[0]}"
+echo "Second river :: ${RIVERS[1]}"
+echo "Third river :: ${RIVERS[2]}"
+
+echo "All rivers :: ${RIVERS[@]}"
