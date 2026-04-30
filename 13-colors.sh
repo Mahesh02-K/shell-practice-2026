747825cf201bec765a857e00559ad587474eccd1
Mahesh02-K <kakuturumahesh@gmail.com>
2026-04-30 16:41:57 +0530

    colors in shell

---
+++ 13-colors.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#check root previleges
+if [ $USERID -eq 0 ]
+then
+    echo -e "You are running with root access ... $Y PROCEED $N"
+else
+    echo -e "$R ERR :: $Y Please run this with root access $N"
+    exit 1
+fi
+
+#verify function takes input as exit status and what command they try to install
+VERIFY(){
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "Installing $2 is ... $G SUCCESS $N"
+    else 
+        echo -e "Installing $2 is ... $R FAILURE $N"
+        exit 1
+    fi
+}
+
+dnf list installed mysql -y
+if [ $? -eq 0 ]
+then 
+    echo -e "Mysql is $Y ALREADY INSTALLED $N ... Nothing to do"
+else
+    echo -e "Mysql is $R NOT INSTALLED $N ... Going to install"
+    dnf install mysql -y
+    VERIFY $? Mysql
+fi
