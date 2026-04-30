e9dc243f1f147ce305508dc9df1eaac7a01c0866
Mahesh02-K <kakuturumahesh@gmail.com>
2026-04-30 16:23:45 +0530

    colors in shell

--- 12-colors.sh
+++ 12-colors.sh
@@ -3,3 +3,9 @@
 echo -e "\e[33m HELLO WORLD"
 
 echo "Hello Everyone"
+
+#whenever we enable color in a line, must be close if we don't need to further lines
+
+echo -e "\e[32m Hi Mahesh \e[0m"
+
+echo -e "How are you?"
