4ee70d8705c716823be82b9fc96cbd7e3b431e84
Mahesh02-K <kakuturumahesh@gmail.com>
2026-05-06 23:15:51 +0530

    variables example

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,7 +4,7 @@ Person1=Trump
 Person2=Musk
 
 echo "$Person1 :: Hey $Person2! how are you?"
-echo "$Person2 :: Hi DJT! I'm good, how about you?"
+echo "$Person2 :: Hi mate! I'm good, how about you?"
 echo "$Person1 :: good! what's going on?"
 echo "$Person2 :: nothing! just planning about mars trip."
 echo "$Person1 :: Oh! super, carry on."
